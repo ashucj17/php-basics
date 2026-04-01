780448e46bb22710ae8f51b02787ee2861b1021e
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2026-04-01 12:11:13 +0530

    var_dump(), isset(), define() and predefined constant

--- 02_variables.php
+++ 02_variables.php
@@ -50,16 +50,83 @@ echo gettype($happy) . "<br/>";
 
 // Print the whole variable
 
+# var_dump is a build-in fuction used for debugging purpose. It shows the value of the variable, variable data type, length of the variable(string, array)
+
+var_dump($name);
+var_dump($age);
+var_dump($salary);
+var_dump($loggedIn);
+var_dump($happy);
+
+var_dump($name, $age, $salary, $loggedIn, $happy);
+
 // Change the value of the variable
+
+$age = 29;
+$loggedIn = "False";
+
 
 // Print type of the variable
 
+echo gettype($age). "<br>";
+echo gettype($loggedIn). "<br>";
+
+var_dump($name . "<br>", $age . "<br>", $salary . "<br>", $loggedIn . "<br>", $happy); // br inside var_dum converts the all the data types into string.
+
+// to get the output in pre writtern formate, use <pre> tag
+
+echo "<pre>";
+
+var_dump($name, $age, $salary, $loggedIn, $happy);
+
+echo "</pre>";
+
+
 // Variable checking functions
+
+/* is_string($name);
+is_int($age);
+is_float($salary);
+is_bool($loggedIn);
+is_null($happy);
+
+these function returns true or false
+
+*/
+var_dump(is_string($name),
+is_int($age),
+is_float($salary),
+is_bool($loggedIn),
+is_null($happy));
+
+echo is_string($name);
+echo is_int($age);
+echo is_float($salary);
+echo is_bool($loggedIn);
+echo is_null($happy);
+echo "<br>";
 
 // Check if variable is defined
 
+# isset function is used to check the variable defined in the given scope or not.
+
+echo isset($name);
+echo isset($fanily);
+var_dump(isset($fanily));
+
 // Constants
 
+define('PI', 3.14);
+echo PI. '<br>';
+
 // Using PHP built-in constants
+
+echo SORT_ASC. '<br>';
+echo PHP_INT_MAX. '<br>';
+
+/* All predefined constangt in php: echo "<pre>";
+print_r(get_defined_constants());
+echo "</pre>"; */
+
 
 ?>
