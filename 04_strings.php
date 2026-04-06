d64c5e62f2d52200093d81eb05d88aebae779025
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2026-04-06 11:49:59 +0530

    string function:  nl2br, htmlentities

--- 04_strings.php
+++ 04_strings.php
@@ -25,6 +25,24 @@ echo "2: " . strchr("Ashish", "i") . "<br>"; // it returns every character given
 
 // Multiline text and line breaks
 
+#nl2br is a tring function which converts new line <br: \n -> <br>
+
+$tree = "This is the parent tree,
+            it have many branches.
+            Many birds have their nest on it.";
+
+            // echo $tree . "<br>";
+            // echo nl2br($tree);
+            // echo nl2br(htmlspecialchars($tree));
+
 // Multiline text and reserve html tags
+$BanyanTree = "This is the <b>oldest</b> tree,
+            it have many <a>branches</a>.
+            Many birds have their <i>nest</i> on it.";
+
+            echo $BanyanTree . "<br>";
+            echo nl2br($BanyanTree) . "<br>";
+            echo htmlentities($BanyanTree) . "<br>";
+
 
 // https://www.php.net/manual/en/ref.strings.php
