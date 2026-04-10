4483d899c556887f217ac368c1a05cbe0cd36b78
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2026-04-10 11:59:12 +0530

    Form validation

--- 14_product_crud/create.php
+++ 14_product_crud/create.php
@@ -1,12 +1,42 @@
 <?php
 
-
-$pdo = new PDO('mysql:host=localhost; port=3306; dbname=product_crud', 'root', '');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// image=&title=&description=&price=
+$errors = [];
 
+$title = "";
+$description = "";
+$price = "";
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $date = date('Y-m-d H:i:s');
+
+    if (!$title) {
+        $errors[] = 'Product title is required';
+    }
+
+    if (!$price) {
+        $errors[] = 'Product price is required';
+    }
+
+    if (empty($errors)) {
+        $statement = $pdo->prepare("INSERT INTO products
+(title, image, description, price, created_at)
+VALUES (:title, :image, :description, :price, :date)");
+
+        $statement->bindValue(':title', $title);
+        $statement->bindValue(':image', '');
+        $statement->bindValue(':description', $description);
+        $statement->bindValue(':price', $price);
+        $statement->bindValue(':date', $date);
+
+        $statement->execute();
+    }
+}
 ?>
 
 
@@ -24,11 +54,15 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 <body>
     <h1>Create New Product</h1>
 
-    <p>
-        <button href="create.php" class="btn btn-success">Create Product</button>
-    </p>
+    <?php if (!empty($errors)): ?>
+        <div class="alert alert-danger">
+            <?php foreach ($errors as $error): ?>
+                <div><?php echo $error ?></div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
 
-    <form action="" method="get">
+    <form action="" method="post" enctype="multipart/form-data">
         <div class="mb-3">
             <label>Product Image</label>
             <br>
@@ -36,17 +70,17 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         </div>
         <div class="mb-3">
             <label>Product Title</label>
-            <input type="text" class="form-control" name="title">
+            <input type="text" class="form-control" name="title" value="<?php echo $title?>">
         </div>
         <div class="mb-3">
             <label>Product Description</label>
-            <textarea class="form-control" name="description"></textarea>
+            <textarea class="form-control" name="description" value="<?php echo $description?>"></textarea>
         </div>
         <div class="mb-3">
             <label>Product Price</label>
-            <input type="number" step='0.01' class="form-control" name="price">
+            <input type="number" step='0.01' class="form-control" name="price" value="<?php echo $price?>">
         </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </body>
 
